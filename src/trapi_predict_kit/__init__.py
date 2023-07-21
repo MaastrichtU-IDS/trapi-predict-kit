e990129ed044f6fae975b91fe009a99ce257ab96
Vincent Emonet <vincent.emonet@gmail.com>
2023-07-21 08:51:03 +0200

    import settings

--- src/trapi_predict_kit/__init__.py
+++ src/trapi_predict_kit/__init__.py
@@ -2,5 +2,6 @@ from .decorators import trapi_predict
 from .loaded_model import LoadedModel, load, save
 from .predict_output import PredictHit, PredictOptions, PredictOutput, TrainingOutput
 from .trapi import TRAPI
+from .config import settings
 
 __version__ = "0.1.0"
