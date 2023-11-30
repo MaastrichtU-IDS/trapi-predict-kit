89373cf6014f4610cb994b265132f586f803cffd
Vincent Emonet <vincent.emonet@gmail.com>
2023-11-30 18:09:29 +0100

    put back score on edge

--- src/trapi_predict_kit/trapi_parser.py
+++ src/trapi_predict_kit/trapi_parser.py
@@ -239,13 +239,13 @@ def resolve_trapi_query(reasoner_query, endpoints_list, infores: str = ""):
                                         "attribute_type_id": "EDAM:data_1048",
                                         "value": model_id_label,
                                     },
-                                    # {
-                                    #     # TODO: use has_confidence_level?
-                                    #     "description": "score",
-                                    #     "attribute_type_id": "EDAM:data_1772",
-                                    #     "value": association_score
-                                    #     # https://www.ebi.ac.uk/ols/ontologies/edam/terms?iri=http%3A%2F%2Fedamontology.org%2Fdata_1772&viewMode=All&siblings=false
-                                    # },
+                                    {
+                                        # TODO: use has_confidence_level?
+                                        "description": "score",
+                                        "attribute_type_id": "EDAM:data_1772",
+                                        "value": association_score
+                                        # https://www.ebi.ac.uk/ols/ontologies/edam/terms?iri=http%3A%2F%2Fedamontology.org%2Fdata_1772&viewMode=All&siblings=false
+                                    },
                                     # https://github.com/NCATSTranslator/ReasonerAPI/blob/1.4/ImplementationGuidance/Specifications/knowledge_level_agent_type_specification.md
                                     {
                                         "attribute_type_id": "biolink:agent_type",
